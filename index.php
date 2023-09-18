8182864a9b7438fce68545ffd28fc80d4ce9dd57
David BEAUMONT <dadid333@gmail.com>
2023-09-18 08:46:26 +0200

    liste des photos sur index

--- index.php
+++ index.php
@@ -28,7 +28,42 @@
     </div>
     <div class="titre-header">
         <!--<h1>Photographe event</h1> -->
-        
+    </div>
+</header>
+<article>
+    <div class="photos_apparentees">
+        <div class="photo_block">
+            <?php 
+            // Récupérer la catégorie de la photo actuellement affichée
+            $current_category = implode(', ', wp_get_post_terms(get_the_ID(), 'categorie', array('fields' => 'names')));
+                
+            // Obtenez l'ID du post actuel
+            $current_post_id = get_the_ID();
+            
+            // On définit les arguments pour définir ce que l'on souhaite récupérer
+            $args = array(
+                'post_type' => 'photo',
+                'order' => 'DESC', // ASC ou DESC 
+                'orderby' => 'date', // title, date, comment_count…
+                'posts_per_page' => 12,
+            );
+            // On exécute la WP Query
+            $my_query = new WP_Query( $args );
+
+            // On lance la boucle !
+            if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+                
+                the_content();
+
+            endwhile;
+            endif;
+
+            // On réinitialise à la requête principale (important)
+            wp_reset_postdata();
+            ?>
+        </div>
+        <input class="" type="submit" value="Charger plus"> 
+
     </div>
 </article>
 <?php get_footer(); ?>
--- single_photo.php
+++ single_photo.php
@@ -14,7 +14,7 @@ $annee=get_field('annee');
 $categories = get_the_term_list(get_the_ID(), 'categorie', '', ', ');
 $formats = get_the_term_list(get_the_ID(), 'format', '', ', ');
 ?> 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article class="single_photo" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header-photo alignwide">
         <?php the_title( '<h1 class="entry-title-photo">', '</h1>' ); ?>
         <ul>
@@ -87,8 +87,41 @@ $formats = get_the_term_list(get_the_ID(), 'format', '', ', ');
 </div>
 <div class="photos_apparentees">
 <h2>vous aimerez aussi</h2>
+    <div class="photo_block">
+    <?php 
+    // Récupérer la catégorie de la photo actuellement affichée
+    $current_category = implode(', ', wp_get_post_terms(get_the_ID(), 'categorie', array('fields' => 'names')));
+        
+    // Obtenez l'ID du post actuel
+    $current_post_id = get_the_ID();
+    
+    // On définit les arguments pour définir ce que l'on souhaite récupérer
+    $args = array(
+        'post_type' => 'photo',
+        'order' => 'ASC', // ASC ou DESC 
+        'orderby' => 'date', // title, date, comment_count…
+        'categorie' => $current_category,
+        'posts_per_page' => 2,
+        'post__not_in' => array($current_post_id), // Exclure le post actuel de la liste
+    );
+    // On exécute la WP Query
+    $my_query = new WP_Query( $args );
 
-    <?php get_template_part('template-parts/content/photo_block');?>
+    // On lance la boucle !
+    if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+        
+        the_content();
+
+    endwhile;
+    endif;
+
+    // On réinitialise à la requête principale (important)
+    wp_reset_postdata();
+    ?>
+
+    </div>
+    
+    <input class="" type="submit" value="Toutes les photos">
 
 </div>
 <?php 
